26febaac967641eff1ed094d17c4dd5e717bf71e
Hijbullah Muhamed Kyrul Amin <hijbullaah@gmail.com>
2019-01-31 15:24:18 +0600

    Multi auth : login logout complete

---
+++ routes/admin.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register admin routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/test', function () {
+    return 'hello';
+});
+
+// Admin Auth Routes
+
+Route::namespace('Backend\Auth')->group(function () {
+    Route::get('/login', 'LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'LoginController@login');
+    Route::post('/logout', 'LoginController@logout')->name('admin.logout');
+});
+
+Route::get('/', 'Backend\AdminController@index')->name('admin.dashboard');
+
